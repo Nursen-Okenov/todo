f058522ab956fd9db0d8c606b5939e8ce078c313
nursen <nursenokenov57@gmail.com>
2020-04-27 03:01:11 +0600

    Create ToDo components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import ToDo from "./containers/ToDo/ToDo"
 function App() {
   return (
     <div className="App">
-      
+      <ToDo />
     </div>
   );
 }
---
+++ src/containers/ToDo/ToDo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./ToDo.module.css";
+
+export default () => {
+    return (
+        <div className={classes.ToDo}>
+            ToDo
+        </div>
+    )
+}
